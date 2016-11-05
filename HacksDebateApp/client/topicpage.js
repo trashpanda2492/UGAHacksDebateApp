3ada712926a1e3073c50277711ac20ab96a46355
KevTheAsian <kjin150@gmail.com>
2016-11-05 10:14:45 -0400

    Add files via upload

--- HacksDebateApp/client/topicpage.js
+++ HacksDebateApp/client/topicpage.js
@@ -3,21 +3,4 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './topic.html';
 
-Template.usrrank.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
 
-Template.usrrank.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.usrrank.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-
-  },
-});
