2a2f5fa119d95b3de9a0ff08abe034dfd9ebc128
kpatel311 <kpatel311@gatech.edu>
2016-11-05 04:56:51 -0400

    Added sample app

--- HacksDebateApp/server/main.js
+++ HacksDebateApp/server/main.js
@@ -6,21 +6,54 @@ import {Topics} from './imports/collections/topics.js'
 
 const PORT = 8080;
 
+function generateUrl() {
+  const text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for( const i=0; i < 8; i++ ) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
 Meteor.startup(() => {
   Meteor.methods({
     'getDebateRoom'(data) {
-      topic = Topics.findOne({"debate.title": data.title});
-      if (data.position == 1 && topic.con.length != 0) {
-        matchedRoom = topic.con[0];
-        Topics.update({"debate.title": data.title}, {
-          $pull:{
-            "debate.$":{
-              {"con":matchedRoom}
+      // When successful match, move matched room to chatrooms from queue
+      topic = Topics.findOne({"title": data.title});
+      if (data.position == 1) {
+        if (topic.con.length != 0) {
+          matchedRoom = topic.con[0];
+          Topics.update({"title": data.title}, {
+            $pull:{
+              "con": matchedRoom
             }
+          });
+          return {"url": matchedRoom, isMatched: true};
+        }
+        const url = generateUrl();
+        Topics.update({"title": data.title}, {
+          $push:{
+            "pro":url
+          }
+        });
+        return {"url": url, isMatched: false};
+      }
+      if (topic.pro.length != 0) {
+        matchedRoom = topic.pro[0];
+        Topics.update({"title": data.title}, {
+          $pull:{
+            "pro": matchedRoom
           }
         });
         return {"url": matchedRoom, isMatched: true};
-      } 
+      }
+      const url = generateUrl();
+      Topics.update({"title": data.title}, {
+        $push:{
+          "con":url
+        }
+      });
+      return {"url": url, isMatched: false};
     }
   });
 
