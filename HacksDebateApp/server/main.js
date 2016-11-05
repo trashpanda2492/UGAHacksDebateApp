70d84841a5e9f9a6047e5dc8bf57a7ca4a1c08a8
kpatel311 <kpatel311@gatech.edu>
2016-11-05 03:22:26 -0400

    Fixed socket.io package problems

--- HacksDebateApp/server/main.js
+++ HacksDebateApp/server/main.js
@@ -1,5 +1,25 @@
 import { Meteor } from 'meteor/meteor';
+import http from 'http';
+import socket_io from 'socket.io';
+
+const PORT = 8080;
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  // Server
+  const server = http.createServer();
+  const io = socket_io(server);
+
+  let counter = 0;
+
+  // New client
+  io.on('connection', function(socket) {
+    console.log('new socket client');
+  });
+
+  // Start server
+  try {
+    server.listen(PORT);
+  } catch (e) {
+    console.error(e);
+  }
 });
